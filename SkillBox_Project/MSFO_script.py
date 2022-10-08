76e9829f55c40d7404f059342b82e635870a679a
borschxporsche <kirill-gruzynov@yandex.ru>
2022-10-08 20:37:20 +0300

    Start

---
+++ SkillBox_Project/MSFO_script.py
@@ -0,0 +1,17 @@
+#script to collect and analyze data from msfo sheets and real stock price
+
+import pypdf2
+import tabula
+import os
+import requests
+
+def get_file(url):
+    pass
+
+def read_content():
+    pass
+
+def analyze_data():
+    pass
+
+
