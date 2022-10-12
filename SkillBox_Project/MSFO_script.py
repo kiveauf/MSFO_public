b15fa41de8283d01c4ded845c8c36f404cb88979
borschxporsche <kirill-gruzynov@yandex.ru>
2022-10-12 23:44:05 +0300

    added clearing functions

--- SkillBox_Project/MSFO_script.py
+++ SkillBox_Project/MSFO_script.py
@@ -1,9 +1,11 @@
 #script to collect and analyze data from msfo sheets and real stock price
 
+from string import whitespace
 import PyPDF2
 import tabula
 import os
 import requests
+import pdfplumber
 
 class Ticker():
     def _init_(self):
@@ -15,28 +17,51 @@ class Ticker():
 def get_file(tkr, url): # take url of the file
     global filename
     file = requests.get(url)
-    tkr.ticker_name = "ticker"#input("Type ticker: ")
+    tkr.ticker_name = "ticker" #input("Type ticker: ")
     filename = f"{tkr.ticker_name}_MSFO.pdf"
     open(filename,'wb').write(file.content)
     print("Done")
 
-def edit_data(text):
+def edit_data(text): #takes whole str doc and return list of str
+    global ed
     clear_text = str()
-    full_stroka = str()
-    condition = True
+    stroki = list()
     for stroka in text.splitlines():
-        #stroka = stroka.strip("  ")
         if len(stroka.strip()) == 0 or stroka == "\n":
-          #  print(f"eto stroka - {stroka}a eto dlina {len(stroka)}")
             stroka = stroka.strip(" \n")
             continue
         if stroka[0].isupper() == True:
-            clear_text += "\n"
-         #   print(f"eto stroka - {stroka}a eto dlina {len(stroka)}")
+            clear_text = del_whitespaces(clear_text)
+            stroki.append(clear_text)
+            clear_text = ""
         clear_text += stroka + " "
-    return clear_text
+        for word in stroka.split():
+            if word in ["тыс.", "тыс", "тысячах", "thousands"]:
+                ed = 1000 #measuring unit
+            if word in ["млн.", "млн", "миллионах", "millions"]:
+                ed = 1000000 #measuring unit
+    print(ed)
+    return stroki
 
-    
+def del_whitespaces(clear_text):
+    while clear_text.count("  ") != 0:
+        whitespaces = clear_text.find("  ")
+        if (whitespaces - 1 > 0) and (whitespaces + 2 < len(clear_text)):
+            if clear_text[whitespaces - 1].isdigit() == False and clear_text[whitespaces + 2].isdigit() == False:
+                clear_text = clear_text.replace("  ", " ", 1)
+            if clear_text[whitespaces - 1].isdigit() == True or clear_text[whitespaces + 2].isdigit() == True:
+                break
+        if (whitespaces == 0) and (whitespaces + 2 < len(clear_text)):
+            if clear_text[whitespaces + 2].isdigit() == False:
+                clear_text = clear_text.replace("  ", " ", 1)
+            if clear_text[whitespaces + 2].isdigit() == True:
+                break
+        if (whitespaces - 1 > 0) and (whitespaces + 2 == len(clear_text)):
+            if clear_text[whitespaces - 1].isdigit() == False:
+                clear_text = clear_text.replace("  ", " ", 1)
+            if clear_text[whitespaces - 1].isdigit() == True:
+                break
+    return clear_text
 
 def read_content(tkr, filename):
     info_pribyl = "No data"
@@ -44,8 +69,9 @@ def read_content(tkr, filename):
     pdf_file = PyPDF2.PdfFileReader(_pdf, strict=False)
     page_dohod = pdf_file.pages[24]
     info = page_dohod.extract_text()
-    info = edit_data(text = info)
-    print(info)
+    info = edit_data(text = info) #info now is list
+    for i in info:
+        print(i)
     #for param in info.splitlines():
     #    if param.startswith("Прибыль за год") == True or param.startswith("Чистая прибыль") == True:
     #        info_pribyl = f"{param}"
@@ -55,6 +81,21 @@ def read_content(tkr, filename):
         
 def analyze_data():
     pass
+
+#def tab(url):
+#    data = tabula.read_pdf(url, pages = '12', stream = True, guess=False)
+#    print(data)
+
+#def plumb(url):
+#    pdf = pdfplumber.open(url)
+#    table_setting={
+#    "vertical_strategy": "text",
+#    "horizontal_strategy": "text",
+#    }
+#    print(pdf.pages[9].extract_table(table_setting)) 
+#tab(url = filename)
+#plumb(url = filename)#сторонние библиотеки для обработки pdf и таблиц
+
 
 docs = ["https://www.magnit.com/upload/iblock/4e4/%D0%905.12_%D0%9F%D0%BE%D0%B4%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F%20%D1%84%D0%B8%D0%BD%D0%B0%D0%BD%D1%81%D0%BE%D0%B2%D0%B0%D1%8F%20%D0%BE%D1%82%D1%87%D0%B5%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%20%D0%90%D0%97_%D0%9C%D0%B0%D0%B3%D0%BD%D0%B8%D1%82_2021%20(%D1%80%D1%83%D1%81%D1%81).pdf",
         "https://mts.ru/upload/contents/10677/mts_ras_fs_21-r.pdf",
