50896e461d73c04db08f48cdf4cc9dc15eaad9a5
borschxporsche <kirill-gruzynov@yandex.ru>
2022-10-18 22:56:36 +0300

    Trying clean up stuff

--- SkillBox_Project/MSFO_script.py
+++ SkillBox_Project/MSFO_script.py
@@ -5,8 +5,8 @@ import requests
 import logging
 import time
 
-#import tabula
-#import pdfplumber
+import tabula
+import pdfplumber
 
 logger = logging.getLogger("PyPDF2")
 logger.setLevel(logging.CRITICAL)
@@ -15,7 +15,7 @@ class Ticker():
     def _init_(self):
         self.ticker_name = str()
         self.pribyl = 0
-        self.viruchka = int()
+        self.viruchka = 0
         self.stock_price = int()
 
 def get_file(tkr, url): # take url of the file
@@ -27,7 +27,7 @@ def get_file(tkr, url): # take url of the file
     return filename
 
 def edit_data(text): #takes whole str doc and return list of str
-    global ed
+    global measure_unit
     clear_text = str()
     stroki = list()
     if len(check(text)) == 0:
@@ -43,12 +43,13 @@ def edit_data(text): #takes whole str doc and return list of str
         clear_text += stroka + " "
         for word in stroka.split():
             if word in ["тыс.", "тыс", "тысячах", "thousands"]:
-                ed = 1000 #measuring unit
+                measure_unit = 1000 #measuring unit
             if word in ["млн.", "млн", "миллионах", "millions"]:
-                ed = 1000000 #measuring unit
+                measure_unit = 1000000 #measuring unit
     return stroki
 
 def edit_whitespaces(clear_text): # clearing line of text
+    prim_list = ["Примечание", "Прим."]
     c_t = str()
     while clear_text.count("  ") != 0:
         whitespaces = clear_text.find("  ")
@@ -94,6 +95,9 @@ def edit_whitespaces(clear_text): # clearing line of text
         #if clear_text[i-1].isalpha() == True and clear_text[i] == "," and clear_text[i+1].isalpha() == True:
         #   c_t += " "
     clear_text = c_t #1 whitespace added among numbers and text
+    for name in prim_list:
+        if clear_text.count(name) >= 1:
+            clear_text
     return clear_text
 
 def check(page): #takes lines of text
@@ -128,29 +132,35 @@ def read_content(filename): #takes file and returns list of string lists
     print(len(pages))
     return pages
         
-def analyze_data(pages):
-    pribyl_list = ["Прибыль за год", "Чистая прибыль"]
-    for page in pages:
-        for param in page:
-            for name in pribyl_list:
-                if param.count(name) != 0:
-                    print(param)
-                    ticker.pribyl = param.split("  ")[1].lstrip()
-                    print(param, "необходимое число -", ticker.pribyl)
+def analyze_data(pages, measuring_unit):
+    viruchka_list = ["Выручка"]
+    for line in pages[1]:
+        for name in viruchka_list:
+            if line.count(name) >= 1:
+                ticker.viruchka = line.split("  ")[1]
+                print(ticker.viruchka)
+            
+    #pribyl_list = ["Прибыль за год", "Чистая прибыль"]
+    #for line in pages[1]:
+    #    for name in pribyl_list:
+    #        if line.count(name) >= 1:
+    #            print(line)
+    #            ticker.pribyl = line.split("  ")[1].lstrip()
+    #            print(line, "необходимое число -", ticker.pribyl)
 
 #def tab(url):
 #    data = tabula.read_pdf(url, pages = '12', stream = True, guess=False)
 #    print(data)
-
 #def plumb(url):
 #    pdf = pdfplumber.open(url)
 #    table_setting={
 #    "vertical_strategy": "text",
 #    "horizontal_strategy": "text",
 #    }
-#    print(pdf.pages[9].extract_table(table_setting)) 
+#    print(pdf.pages[12].extract_table(table_setting)) 
+#plumb(url = filename)
 #tab(url = filename)
-#plumb(url = filename)#сторонние библиотеки для обработки pdf и таблиц
+#сторонние библиотеки для обработки pdf и таблиц
 
 
 docs = ["https://www.magnit.com/upload/iblock/4e4/%D0%905.12_%D0%9F%D0%BE%D0%B4%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F%20%D1%84%D0%B8%D0%BD%D0%B0%D0%BD%D1%81%D0%BE%D0%B2%D0%B0%D1%8F%20%D0%BE%D1%82%D1%87%D0%B5%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%20%D0%90%D0%97_%D0%9C%D0%B0%D0%B3%D0%BD%D0%B8%D1%82_2021%20(%D1%80%D1%83%D1%81%D1%81).pdf",
@@ -162,8 +172,9 @@ file_url = docs[0]
 ticker = Ticker()
 filename = str()
 pages = list()
+measure_unit = int()
 
-start_time = time.time() #checking how long code executes
+#start_time = time.time() #checking how long code executes
 
 filename = get_file(tkr = ticker, url = file_url)
 data = read_content(filename)
@@ -171,8 +182,8 @@ for page in data:
     for line in page:
         print(line)
     print("____________________________")
-print(f"--- {time.time() - start_time} seconds ---")
+#print(f"--- {time.time() - start_time} seconds ---")
 
-#analyze_data(data)
+#analyze_data(data, measure_unit)
 
 
