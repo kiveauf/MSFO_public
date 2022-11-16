750c4a7d13e691fc514ef4f9a393c2ebce311698
borschxporsche <kirill-gruzynov@yandex.ru>
2022-11-16 22:02:28 +0300

    Some improvments

--- SkillBox_Project/MSFO_script.py
+++ SkillBox_Project/MSFO_script.py
@@ -34,10 +34,28 @@ def get_file(tkr, url): # take url of the file
     print("Done")
     return filename
 
+def read_content(filename): #takes file and returns list of string lists
+    print("Reading content")
+    _pdf = open(filename, 'rb')
+    pdf_file = PyPDF2.PdfFileReader(_pdf, strict=False)
+    page_dohod = pdf_file.pages
+    for page in page_dohod:
+        info = page.extract_text()
+        info = edit_data(text = info) #info now is list
+        if len(info) != 0 and len(pages) < 4:
+            pages.append(info)
+    print("Content ready")
+    if len(pages) != 4:
+        print("Есть ошибки прочтения")
+        print(f"Количество страниц - {len(pages)}")
+        return[]
+    return pages
+
 def edit_data(text): #takes whole str page and return list of str
-    global measure_unit
     clear_text = str()
     stroki = list()
+    if ticker.amount == 0:
+           find_amount(text)
     if len(check(text)) == 0: #checks if the page is in the list
         return []
     for stroka in text.splitlines():
@@ -49,32 +67,48 @@ def edit_data(text): #takes whole str page and return list of str
             stroki.append(clear_text)
             clear_text = ""
         clear_text += stroka + " " #adding words to one line
-        for word in stroka.split():
-            if word in ["тыс.", "тыс", "тысячах", "thousands"]:
-                measure_unit = 1000 #measuring unit
-            if word in ["млн.", "млн", "миллионах", "millions", "Млн"]:
-                measure_unit = 1000000 #measuring unit
+        know_measure(stroka)
     return stroki
 
-def edit_whitespaces(clear_text): # clearing line of text
+def check(page): #takes lines of text
+    report = ["Консолидированный отчет о финансовом положении", "БУХГАЛТЕРСКИЙ БАЛАНС", "Консолидированный отчет о финансовом положении",
+              "Консолидированный отчет о совокупном доходе", "ОТЧЕТ О ФИНАНСОВЫХ РЕЗУЛЬТАТАХ", "Консолидированный отчет о прибылях и убытках и прочем совокупном доходе",
+              "Консолидированный отчет о движении денежных средств", "ОТЧЕТ ОБ ИЗМЕНЕНИЯХ КАПИТАЛА", "Консолидированный отчет об изменениях в капитале",
+              "Консолидированный отчет об изменениях в капитале", "ОТЧЕТ О ДВИЖЕНИИ ДЕНЕЖНЫХ СРЕДСТВ", "Консолидированный отчет о движении денежных средств",
+              "Консолидированный отчет о прибыли или убытке", "Консолидированный отчет о финансовом положении",
+              "Консолидированный Отчет о финансовом положении", "Консолидированный Отчет о прибылях и убытках", "Консолидированный Отчет о совокупном доходе", 
+              "Консолидированный Отчет о движении денежных средств"
+              ]
+    oglavlenie = ["Содержание", "СОДЕРЖАНИЕ"]
+    for stroka in page.splitlines():
+        for line in oglavlenie:
+            if " ".join(stroka.split()).count(line) >= 1:
+                return []
+        for headline in report:
+            if " ".join(stroka.split()).count(headline) >= 1:
+                return page
+    return []
+
+def edit_whitespaces(clear_text): #clearing line of text
     clear_text = clear_text.replace("   ", "  ")
     if len(clear_text) != 0:
         c_t = str()
         x = 0
-        whitespaces = 0
+        y = 0
+        start = 0
         amount_ws = clear_text.count("  ") + 1
         while x != amount_ws:
             x += 1
-            start = whitespaces
             whitespaces = clear_text.find("  ", start)
+            start = whitespaces
             if (whitespaces - 1 >= 0) and (whitespaces + 2 < len(clear_text)):
-                #print(clear_text[0:whitespaces], "!!", clear_text[whitespaces:len(clear_text)])
                 if clear_text[whitespaces - 1].isdigit() == False and clear_text[whitespaces + 2].isdigit() == False:
                     clear_text = replace_whitespace(clear_text, whitespaces)
                     continue
-                if is_here_alnum(clear_text[0:whitespaces]) == "alnum" and is_here_alnum(clear_text[whitespaces:len(clear_text)]) == "digit":
-                    clear_text = replace_whitespace(clear_text, whitespaces)
-                    continue
+                #if is_here_alnum(clear_text[0:whitespaces]) == "alnum" and is_here_alnum(clear_text[whitespaces:len(clear_text)]) == "digit" and notes == True:
+                #    clear_text = replace_whitespace(clear_text, whitespaces)
+                #    y = 1
+                #    continue
             elif (whitespaces == 0) and (whitespaces + 2 < len(clear_text)):
                 if clear_text[whitespaces + 2].isdigit() == False:
                     clear_text = replace_whitespace(clear_text, whitespaces)
@@ -101,79 +135,45 @@ def edit_whitespaces(clear_text): # clearing line of text
             c_t += clear_text[i]
             if clear_text[i] == " " and clear_text[i-1].isalpha() == True and clear_text[i+1] == "(" and clear_text[i+2].isdigit() == True:
                c_t += " "
-            elif clear_text[i] == " " and clear_text[i-1].isalpha() == True and clear_text[i+1].isdigit() == True:
+            elif clear_text[i] == " " and clear_text[i-1].isalpha() == True and clear_text[i+1].isdigit() == True and is_here_alnum(clear_text[i:len(clear_text)]) == "digit":
                c_t += " "
             elif clear_text[i] == " " and clear_text[i-1] == ")" and clear_text[i+1] == "(":
                c_t += " "
             elif clear_text[i] == " " and clear_text[i-1] == "." and clear_text[i+1].isdigit() == True:
+               c_t += " "
+            elif clear_text[i] == " " and clear_text[i-1] == ")" and is_here_alnum(clear_text[i:len(clear_text)]) == "digit" and clear_text[i+1] != " ":
                c_t += " "
         clear_text = c_t.strip() + clear_text[len(clear_text) - 1] #1 whitespace added among numbers and text
         return clear_text
     else:
         return ""
 
-def check(page): #takes lines of text
-    report = ["Консолидированный отчет о финансовом положении", "БУХГАЛТЕРСКИЙ БАЛАНС", "Консолидированный отчет о финансовом положении",
-              "Консолидированный отчет о совокупном доходе", "ОТЧЕТ О ФИНАНСОВЫХ РЕЗУЛЬТАТАХ", "Консолидированный отчет о прибылях и убытках и прочем совокупном доходе",
-              "Консолидированный отчет о движении денежных средств", "ОТЧЕТ ОБ ИЗМЕНЕНИЯХ КАПИТАЛА", "Консолидированный отчет об изменениях в капитале",
-              "Консолидированный отчет об изменениях в капитале", "ОТЧЕТ О ДВИЖЕНИИ ДЕНЕЖНЫХ СРЕДСТВ", "Консолидированный отчет о движении денежных средств",
-              "Консолидированный отчет о прибыли или убытке", "Консолидированный отчет о финансовом положении",
-              "Консолидированный Отчет о финансовом положении", "Консолидированный Отчет о прибылях и убытках", "Консолидированный Отчет о совокупном доходе", 
-              "Консолидированный Отчет о движении денежных средств"
-              ]
-    oglavlenie = ["Содержание", "СОДЕРЖАНИЕ"]
-    for stroka in page.splitlines():
-        for line in oglavlenie:
-            if " ".join(stroka.split()).count(line) >= 1:
-                return []
-        for headline in report:
-            if " ".join(stroka.split()).count(headline) >= 1:
-                return page
-        if ticker.amount == 0:
-           find_amount(stroka)
-    return []
-
-def read_content(filename): #takes file and returns list of string lists
-    print("Reading content")
-    _pdf = open(filename, 'rb')
-    pdf_file = PyPDF2.PdfFileReader(_pdf, strict=False)
-    page_dohod = pdf_file.pages
-    for page in page_dohod:
-        info = page.extract_text()
-        info = edit_data(text = info) #info now is list
-        if len(info) != 0 and len(pages) < 4:
-            pages.append(info)
-    print("Content ready")
-    if len(pages) != 4:
-        print("Есть ошибки прочтения")
-        print(f"Количество страниц - {len(pages)}")
-        return[]
-    return pages
-        
-def collect_data(pages, measuring_unit):
+def collect_data(pages, measuring_unit): #reads the page and finds needed params
     print("Find necessary data")
     viruchka_list = ["Выручка"]
     pribyl_list = ["Прибыль за год", "Чистая прибыль отчетного периода", "Прибыль за отчетный год"]
     for line in pages[1]:
+        #notes = know_notes(line)
         for name in viruchka_list:
             if line.count(name) >= 1 and is_here_num(line) == True:
                 print(line)
-                viruchka_parts = line.split("  ")[1].split()
-                ticker.viruchka = int("".join(viruchka_parts[1:len(viruchka_parts)])) * measuring_unit
+                viruchka_line = edit_line(line).split("  ")[0]
+                viruchka = "".join(viruchka_line.split())
+                ticker.viruchka = int(viruchka) * measuring_unit
                 print(f"Выручка - {ticker.viruchka}")
         for name in pribyl_list:
             if line.count(name) >= 1 and is_here_num(line) == True:
                 print(line)
                 ticker.pribyl = int("".join(line.split("  ")[1].split())) * measuring_unit
-                #print(f"Прибыль - {ticker.pribyl}")
+                print(f"Прибыль - {ticker.pribyl}")
 
-def is_here_num(line):
+def is_here_num(line): #if line contains number
     for i in line.strip():
         if i.isdigit() == True:
             return True
     return False
 
-def is_here_alnum(line):
+def is_here_alnum(line): #tells what line contains
     counter = [False, False]
     for i in line.strip():
         if i.isdigit() == True:
@@ -189,13 +189,33 @@ def is_here_alnum(line):
         return "alpha"
     return "other"
 
-def replace_whitespace(line, index):
+def edit_line(line): # do some editing with line containing viruchku
+    viruchka_parts = line.partition("  ")[2].strip()
+    space = viruchka_parts.find(" ")
+    viruchka_parts = viruchka_parts[space:len(viruchka_parts)].strip()
+    return viruchka_parts
+
+def replace_whitespace(line, index): #remove one whitespace from double whitespaces
     newline = str()
     for i in range(len(line)-1):
         if i == index:
             continue
         newline += line[i]
     return newline
+
+def know_notes(line): #checks if there notes
+    notes_list = ["Примечание", "Поясн", "Прим"]
+    for word in notes_list:
+        if line.find(word) != 0:
+            return True
+
+def know_measure(line):
+    global measure_unit
+    for word in line.split():
+            if word in ["тыс.", "тыс", "тысячах", "thousands"]:
+                measure_unit = 1000 #measuring unit
+            if word in ["млн.", "млн", "миллионах", "millions", "Млн"]:
+                measure_unit = 1000000 #measuring unit
         
 def print_pages(pages):
     for page in pages:
@@ -227,32 +247,45 @@ def parser(): #using selenium to get all info because of javascript
     #amount_sel = page.find_elements(By.CLASS_NAME, 'Yai9')
     #ticker.amount = float(amount_sel[5].text.replace(" ", "").replace(",", "."))
 
-def find_amount(line): #find amount of shares
+def find_amount(text): #find amount of shares
     global amount_line
-    measure_unit_list = ["в тысячах", "тысяч"]
+    measure_unit_thousands_list = ["в тысячах", "тысяч"]
+    measure_unit_mil_list = ["в млн", "млн"]
     shares_amount_list = ["Средневзвешенное количество выпущенных обыкновенных акций", "Средневзвешенное количество обыкновенных акций",
-                          "Средневзвешенное количество акций"]
-    if line[0].isupper() == True:
-        amount_line = line 
-    if is_here_num(line) == False and line[0].isupper() == False:
-        amount_line += line
-    if is_here_num(line) == True and line[0].isupper() == False:
-        amount_line += line
-    if is_here_num(line) == True:
-        for name in shares_amount_list:
-            if amount_line.count(name) >= 1:
-                amount_line = edit_whitespaces(amount_line)
-                for mu in measure_unit_list:
-                    if amount_line.count(mu) >= 1:
-                        ticker.amount = float(amount_line.strip().split("  ")[1].replace(" ", "").replace(",", "")) * 1000
+                          "Средневзвешенное количество акций", "Средневзвешенное количество акций"]
+    for line in text.splitlines():
+        line = line.strip()
+        #print(line)
+        if len(line) != 0:
+            if line[0].isupper() == True:
+                amount_line = line
+            if is_here_num(line) == False and line[0].isupper() == False:
+                amount_line += " " + line
+            if is_here_num(line) == True and line[0].isupper() == False:
+                amount_line += " " + line
+            if is_here_num(line) == True and amount_line[0].isupper() == True:
+                for name in shares_amount_list:
+                    if amount_line.count(name) >= 1:
+                        amount_line = edit_whitespaces(amount_line)
+                        print(amount_line)
+                        for mu in measure_unit_thousands_list:
+                            if amount_line.count(mu) >= 1:
+                                ticker.amount = float(amount_line.strip().split("  ")[1].replace(" ", "").replace(",", "")) * 1000
+                                print(f"Количество акций - {ticker.amount} шт.")
+                                return True
+                        for mu in measure_unit_mil_list:
+                            if amount_line.count(mu) >= 1:
+                                ticker.amount = float(amount_line.strip().split("  ")[1].replace(" ", "").replace(",", "")) * 1000000
+                                print(f"Количество акций - {ticker.amount} шт.")
+                                return True
+                        ticker.amount = float(amount_line.strip().split("  ")[1].replace(" ", "").replace(",", ""))
+                        print(f"Количество акций - {ticker.amount} шт.")
                         return True
-                ticker.amount = float(amount_line.strip().split("  ")[1].replace(" ", "").replace(",", ""))
-                return True
 
 def tinkoff_api():
     pass
 
-def analyze_data():
+def analyze_data(): #just do the math
     ticker.capitalization = ticker.price * ticker.amount
     if ticker.pribyl != 0:
         ticker.pe = ticker.capitalization / ticker.pribyl
