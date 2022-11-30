b2e965fcb12fbbe127b95176c85c328befa8d871
borschxporsche <kirill-gruzynov@yandex.ru>
2022-11-30 22:27:30 +0300

    Some clearing

--- SkillBox_Project/MSFO_script.py
+++ SkillBox_Project/MSFO_script.py
@@ -1,18 +1,20 @@
-#script to collect and analyze data from msfo sheets and real stock price
+"""
+script to collect and analyze data from msfo sheets and real stock price
+use python 3.9
+"""
 
 import PyPDF2
 import requests
 import logging
 import time
-import bs4
-from requests_html import HTMLSession
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import os
 from tinkoff.invest import Client
 from dotenv import load_dotenv
-
+from DocsTest import docs
+                                             
 load_dotenv() 
 logger = logging.getLogger("PyPDF2")
 logger.setLevel(logging.CRITICAL)
@@ -328,15 +330,6 @@ def run(url, name, method):#just run the program
     analyze_data()
     print_data()
     return (len(pages), ticker.pe, ticker.ps)
-
-
-docs = [
-        "https://www.magnit.com/upload/iblock/4e4/%D0%905.12_%D0%9F%D0%BE%D0%B4%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F%20%D1%84%D0%B8%D0%BD%D0%B0%D0%BD%D1%81%D0%BE%D0%B2%D0%B0%D1%8F%20%D0%BE%D1%82%D1%87%D0%B5%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%20%D0%90%D0%97_%D0%9C%D0%B0%D0%B3%D0%BD%D0%B8%D1%82_2021%20(%D1%80%D1%83%D1%81%D1%81).pdf",
-        "https://mts.ru/upload/contents/10677/mts_ras_fs_21-r.pdf",
-        "https://acdn.tinkoff.ru/static/documents/223e5d7f-6d12-429f-aae1-a25b154ea3e2.pdf",
-        "https://lukoil.ru/FileSystem/9/577502.pdf",
-        "http://www.rushydro.ru/upload/iblock/89e/IFRS-RusHydro_2112_rrus.pdf"
-        ]
 
 ticker = Ticker()
 filename = str()
--- SkillBox_Project/MSFO_script_test.py
+++ SkillBox_Project/MSFO_script_test.py
@@ -1,5 +1,6 @@
 import unittest
 import MSFO_script
+from DocsTest import docs
 
 class Test_test_1(unittest.TestCase):
     def setUp(self):
@@ -11,7 +12,7 @@ class Test_test_1(unittest.TestCase):
         MSFO_script.method = str()
 
     def test_magnit_parsing(self):
-        url = "https://www.magnit.com/upload/iblock/4e4/%D0%905.12_%D0%9F%D0%BE%D0%B4%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F%20%D1%84%D0%B8%D0%BD%D0%B0%D0%BD%D1%81%D0%BE%D0%B2%D0%B0%D1%8F%20%D0%BE%D1%82%D1%87%D0%B5%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%20%D0%90%D0%97_%D0%9C%D0%B0%D0%B3%D0%BD%D0%B8%D1%82_2021%20(%D1%80%D1%83%D1%81%D1%81).pdf"
+        url = docs[0]
         print("magnit_parsing")
         result = MSFO_script.run(url, "mgnt", "p")
         self.assertEqual(result[0], 4)
@@ -19,7 +20,7 @@ class Test_test_1(unittest.TestCase):
         self.assertTrue(result[2] != 0)
     
     def test_mts_parsing(self):
-        url = "https://mts.ru/upload/contents/10677/mts_ras_fs_21-r.pdf"
+        url = docs[1]
         print("mts_parsing")
         result = MSFO_script.run(url, "mtss", "p")
         self.assertEqual(result[0], 4)
@@ -27,7 +28,7 @@ class Test_test_1(unittest.TestCase):
         self.assertTrue(result[2] != 0)
         
     def test_rushydro_parsing(self):
-        url = "http://www.rushydro.ru/upload/iblock/89e/IFRS-RusHydro_2112_rrus.pdf"
+        url = docs[4]
         print("hydro_parsing")
         result = MSFO_script.run(url, "hydr", "p")
         self.assertEqual(result[0], 4)
@@ -35,7 +36,7 @@ class Test_test_1(unittest.TestCase):
         self.assertTrue(result[2] != 0)
     
     def test_magnit_tinkoff_api(self):
-        url = "https://www.magnit.com/upload/iblock/4e4/%D0%905.12_%D0%9F%D0%BE%D0%B4%D0%BF%D0%B8%D1%81%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F%20%D1%84%D0%B8%D0%BD%D0%B0%D0%BD%D1%81%D0%BE%D0%B2%D0%B0%D1%8F%20%D0%BE%D1%82%D1%87%D0%B5%D1%82%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D1%81%20%D0%90%D0%97_%D0%9C%D0%B0%D0%B3%D0%BD%D0%B8%D1%82_2021%20(%D1%80%D1%83%D1%81%D1%81).pdf"
+        url = docs[0]
         print("magnit_tinkoff")
         result = MSFO_script.run(url, "mgnt", "t")
         self.assertEqual(result[0], 4)
@@ -43,7 +44,7 @@ class Test_test_1(unittest.TestCase):
         self.assertTrue(result[2] != 0)
     
     def test_mts_tinkoff_api(self):
-        url = "https://mts.ru/upload/contents/10677/mts_ras_fs_21-r.pdf"
+        url = docs[1]
         print("mts_tinkoff")
         result = MSFO_script.run(url, "mtss", "t")
         self.assertEqual(result[0], 4)
@@ -51,7 +52,7 @@ class Test_test_1(unittest.TestCase):
         self.assertTrue(result[2] != 0)
         
     def test_rushydro_tinkoff_api(self):
-        url = "http://www.rushydro.ru/upload/iblock/89e/IFRS-RusHydro_2112_rrus.pdf"
+        url = docs[4]
         print("hydro_tinkoff")
         result = MSFO_script.run(url, "hydr", "t")
         self.assertEqual(result[0], 4)
