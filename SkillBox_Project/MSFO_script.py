c38e6c7197fc7d1729e059ec3d402eef53b3b8f7
borschxporsche <kirill-gruzynov@yandex.ru>
2023-01-31 00:43:24 +0300

    WIP

--- SkillBox_Project/MSFO_script.py
+++ SkillBox_Project/MSFO_script.py
@@ -75,28 +75,23 @@ def read_content(filename): #takes file and returns dict.
         page_dohod = pdf_file.pages
         for page in page_dohod:
             info = page.extract_text() #just strings
-            if read_data(info) == True: #check if page contains needed tables
+            if read_data(info, counter, filename) == True: #check if page contains needed tables
                 number_pages.append(str(counter))
             counter += 1
     #print("Content ready")
     number_pages = ", ".join(number_pages)
-    #print(number_pages)
-    content_pages = camel(filename, number_pages) #now dict with page info
-    #if len(content_pages) != 4:
-    #    print("Есть ошибки прочтения")
-    #    print(f"Количество страниц - {len(pages)}")
-    #    return[]
+    content_pages = camel(filename, number_pages, ['60,645,550,40']) #now dict with page info
     for i in content_pages.items():
         print(i)
     return content_pages
 
-def camel(filename, pages): #takes file and number of pages and returns 
+def camel(filename, pages, table_areas): #takes file and number of pages and returns 
     pages_list_raw = list()
     pages_dicts = dict()
-    pdf_file = camelot.read_pdf(filename, flavor = "stream", pages = pages, table_areas=['60,645,550,40'])
+    pdf_file = camelot.read_pdf(filename, flavor = "stream", pages = pages, table_areas = table_areas)
     for i in pdf_file:
-        #camelot.plot(i, kind = 'contour').show()
-        #matplotlib.pyplot.show(block=True)
+        camelot.plot(i, kind = 'contour').show()
+        matplotlib.pyplot.show(block=True)
         result = i.df
         pd.set_option('display.max_rows', 500)
         pd.set_option('display.max_columns', 2000)
@@ -105,30 +100,21 @@ def camel(filename, pages): #takes file and number of pages and returns
         #print(result_dict)
         for i in result_dict.items(): 
             pages_list_raw.append(i)
+    for i in pages_list_raw:
+        print(i)
     pages_dicts = take_info(pages_list_raw)
     return pages_dicts
 
-def read_data(text): #takes whole str page and return tuple
+def read_data(text, counter, filename): #takes whole str page and return tuple
     if ticker.amount == 0:
-           find_amount(text)
+           find_amount(text, counter, filename)
     if check(text) == False: #checks if the page is in the list
         return False
-    #for stroka in text.splitlines():
-    #    if len(stroka.strip()) == 0 or stroka == "\n":
-    #        stroka = stroka.strip(" \n")
-    #        continue
-    #    if stroka[0].isupper() == True:
-    #        clear_text = edit_whitespaces(clear_text) #now is tuple (info, data x-year, data y-year)
-    #        print(clear_text)
-    #        stroki.setdefault(clear_text[0],(clear_text[1],clear_text[2]))
-    #        clear_text = ""
-    #    clear_text += stroka + " " #adding words to one line
-    #    know_measure(stroka)
+    know_measure(text)
     return True
 
-
 #TODO algo to now what actual number to get
-def take_info(pages_raw): #takes dict of pages and returns dict with info
+def take_info(pages_raw): #takes dict of pages and returns dict (info: latest number)
     pages_dict = dict()
     for line in pages_raw:
         if line[1].get(2) == "":
@@ -157,132 +143,6 @@ def check(page): #takes lines of text
                 #print(stroka)
                 return True
     return False
-
-#def edit_whitespaces(clear_text): #clearing line of text, return tuple (info, data x-year, data y-year)
-#    print(clear_text)
-#    whitespace_to_delete_list = list()
-#    whitespace_to_add_list = list()
-#    if len(clear_text) != 0:
-#        clear_text = clear_text.strip()
-#        amount_whitespaces = clear_text.count(" ")
-#        start = 0
-#        x = 0
-#        while x != amount_whitespaces:
-#            whitespace = clear_text.find(" ", start)
-#            start = whitespace + 1
-#            x += 1
-#            if alldigit(clear_text[whitespace:len(clear_text)]) == False:
-#                if clear_text[whitespace + 1] == " ":
-#                    whitespace_to_delete_list.append(whitespace)
-#                    continue
-#            else:
-#                if clear_text[whitespace + 1] == " " and clear_text[whitespace + 2] == "(" and clear_text[whitespace + 3].isdigit() == True:
-#                    continue
-#                if clear_text[whitespace + 1] == " " and clear_text[whitespace + 2].isdigit() == True:
-#                    continue
-#                if clear_text[whitespace - 1] == ")" and clear_text[whitespace + 1] == "(":
-#                    whitespace_to_add_list.append(whitespace)
-#                    continue
-#                if clear_text[whitespace - 1] != " " and clear_text[whitespace + 1] == "(" and clear_text[whitespace + 2].isdigit() == True:
-#                    whitespace_to_add_list.append(whitespace)
-#                    continue
-#                if clear_text[whitespace + 1] == " ":
-#                    whitespace_to_delete_list.append(whitespace)
-#                    continue
-#            #if clear_text[whitespace - 1].isdigit() == False and clear_text[whitespace + 1] == " " and clear_text[whitespace + 2].isdigit() == False:
-#            #    whitespace_list.append(whitespace)
-#            #    continue
-#            #if clear_text[whitespace - 1].isdigit() == True and clear_text[whitespace + 1] == " " and clear_text[whitespace + 2].isdigit() == True:
-#            #    whitespace_list.append(whitespace)
-#            #    continue
-#            #if clear_text[whitespace - 1].isdigit() == True and clear_text[whitespace + 1] == " " and clear_text[whitespace + 2].isdigit() == False:
-#            #    whitespace_list.append(whitespace)
-#            #    continue
-#            #if clear_text[whitespace - 1].isdigit() == True and clear_text[whitespace + 1] == " " and clear_text[whitespace + 2].isdigit() == True:
-#            #    whitespace_list.append(whitespace)
-#            #    continue
-#        clear_text = edit_whitespace(clear_text, whitespace_to_delete_list, whitespace_to_add_list)
-#        #for i in range(len(clear_text) - 1):
-#        #    if clear_text[i] == " " and clear_text[i-1].isalpha() == True and clear_text[i+1] == ",":
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i+1] == "/" and clear_text[i-1].isalpha() == True:
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i-1] == "/" and clear_text[i+1].isalpha() == True:
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i-1] == ")" and clear_text[i+1] == "/":
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i-1] == "/" and clear_text[i+1] == "(":
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i-1] == "(" and clear_text[i+1].isalpha() == True:
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i+1] == ")" and clear_text[i-1].isalpha() == True:
-#        #       continue
-#        #    c_t += clear_text[i]
-#        #    if clear_text[i] == " " and clear_text[i-1].isalpha() == True and clear_text[i+1] == "(" and clear_text[i+2].isdigit() == True:
-#        #       c_t += " "
-#        #    elif clear_text[i] == " " and clear_text[i-1].isalpha() == True and clear_text[i+1].isdigit() == True and is_here_alnum(clear_text[i:len(clear_text)]) == "digit":
-#        #       c_t += " "
-#        #    elif clear_text[i] == " " and clear_text[i-1] == ")" and clear_text[i+1] == "(":
-#        #       c_t += " "
-#        #    elif clear_text[i] == " " and clear_text[i-1] == "." and clear_text[i+1].isdigit() == True:
-#        #       c_t += " "
-#        #    elif clear_text[i] == " " and clear_text[i-1] == ")" and is_here_alnum(clear_text[i:len(clear_text)]) == "digit" and clear_text[i+1] != " ":
-#        #       c_t += " "
-#        #clear_text = c_t.strip() + clear_text[len(clear_text) - 1] #1 whitespace added among numbers and text
-#        #while x != amount_ws:
-#        #    x += 1
-#        #    whitespaces = clear_text.find("  ", start)
-#        #    start = whitespaces #index of the first whitespace
-#        #    if (whitespaces - 1 >= 0) and (whitespaces + 2 < len(clear_text)): #if whitespace isn't first and third from the end
-#        #        if clear_text[whitespaces - 1].isdigit() == False and clear_text[whitespaces + 2].isdigit() == False:
-#        #            clear_text = replace_whitespace(clear_text, whitespaces)
-#        #            continue
-#        #    elif (whitespaces == 0) and (whitespaces + 2 < len(clear_text)): #if whitespace isn't first and third from the end
-#        #        if clear_text[whitespaces + 2].isdigit() == False:
-#        #            clear_text = replace_whitespace(clear_text, whitespaces)
-#        #            continue
-#        #    elif (whitespaces - 1 >= 0) and (whitespaces + 2 == len(clear_text) - 1): #if whitespace isn't first and third from the end
-#        #        if clear_text[whitespaces - 1].isdigit() == False:
-#        #            clear_text = replace_whitespace(clear_text, whitespaces)
-#        #            continue
-#        #for i in range(len(clear_text) - 1):
-#        #    if clear_text[i] == " " and clear_text[i-1].isalpha() == True and clear_text[i+1] == ",":
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i+1] == "/" and clear_text[i-1].isalpha() == True:
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i-1] == "/" and clear_text[i+1].isalpha() == True:
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i-1] == ")" and clear_text[i+1] == "/":
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i-1] == "/" and clear_text[i+1] == "(":
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i-1] == "(" and clear_text[i+1].isalpha() == True:
-#        #       continue
-#        #    if clear_text[i] == " " and clear_text[i+1] == ")" and clear_text[i-1].isalpha() == True:
-#        #       continue
-#        #    c_t += clear_text[i]
-#        #    if clear_text[i] == " " and clear_text[i-1].isalpha() == True and clear_text[i+1] == "(" and clear_text[i+2].isdigit() == True:
-#        #       c_t += " "
-#        #    elif clear_text[i] == " " and clear_text[i-1].isalpha() == True and clear_text[i+1].isdigit() == True and is_here_alnum(clear_text[i:len(clear_text)]) == "digit":
-#        #       c_t += " "
-#        #    elif clear_text[i] == " " and clear_text[i-1] == ")" and clear_text[i+1] == "(":
-#        #       c_t += " "
-#        #    elif clear_text[i] == " " and clear_text[i-1] == "." and clear_text[i+1].isdigit() == True:
-#        #       c_t += " "
-#        #    elif clear_text[i] == " " and clear_text[i-1] == ")" and is_here_alnum(clear_text[i:len(clear_text)]) == "digit" and clear_text[i+1] != " ":
-#        #       c_t += " "
-#        #clear_text = c_t.strip() + clear_text[len(clear_text) - 1] #1 whitespace added among numbers and text
-#        print(clear_text)
-#        parts = clear_text.rsplit("  ", maxsplit = 2)
-#        if len(parts) == 3:
-#            part1 = parts[0]
-#            part2 = parts[1]
-#            part3 = parts[2]
-#            return (part1, part2, part3) 
-#        else: 
-#            return (parts[0],"","")  # TODO if cannot properly read, make some func mb
-#    else:
-#        return ("","","")
 
 def collect_data(pages, measuring_unit): #reads the page and finds needed params
     #print("Find necessary data")
@@ -321,45 +181,20 @@ def is_here_alnum(line): #tells what line contains
         return "alpha"
     return "other"
 
-def alldigit(line):
-    edited_line = "".join(line.split())
-    edited_line = edited_line.replace("(","")
-    edited_line = edited_line.replace(")","")
-    if edited_line.isdigit() == True:
-        return True
-    return False
-
-#def edit_line(line): # do some editing with line containing viruchku
-#    viruchka_parts = line.partition("  ")[2].strip()
-#    space = viruchka_parts.find(" ")
-#    viruchka_parts = viruchka_parts[space:len(viruchka_parts)].strip()
-#    return viruchka_parts
-
-def edit_whitespace(line, index_list_to_delete, index_list_to_add): #remove one whitespace from double whitespaces
-    patch = list(line)
-    counter = 0
-    #if 
-    for i in index_list_to_delete:
-        patch.pop(i + counter)
-        counter -= 1
-    for i in index_list_to_add:
-        patch.insert(i + counter, " ")
-        counter += 1
-    return "".join(patch)
-
 def know_notes(line): #checks if there notes
     notes_list = ["Примечание", "Поясн", "Прим"]
     for word in notes_list:
         if line.find(word) != 0:
             return True
 
-def know_measure(line):
+def know_measure(text):
     global measure_unit
-    for word in line.split():
-            if word in ["тыс.", "тыс", "тысячах", "thousands"]:
-                measure_unit = 1000 #measuring unit
-            if word in ["млн.", "млн", "миллионах", "millions", "Млн"]:
-                measure_unit = 1000000 #measuring unit
+    for line in text:
+        for word in line.split():
+                if word in ["тыс.", "тыс", "тысячах", "thousands"]:
+                    measure_unit = 1000 #measuring unit
+                if word in ["млн.", "млн", "миллионах", "millions", "Млн"]:
+                    measure_unit = 1000000 #measuring unit
         
 def print_pages(pages):
     for page in pages:
@@ -392,9 +227,10 @@ def parser(): #using selenium to get all info because of javascript
     #amount_sel = page.find_elements(By.CLASS_NAME, 'Yai9')
     #ticker.amount = float(amount_sel[5].text.replace(" ", "").replace(",", "."))
 
-def find_amount(text): #find amount of shares  #TODOOOOO
-    global counter
-    amount_line = str()
+def find_amount(text, counter, filename): #find amount of shares 
+    amount_key = str()
+    amount_value = int()
+    #amount_line = str()
     measure_unit_thousands_list = ["в тысячах", "тысяч"]
     measure_unit_mil_list = ["в млн", "млн"]
     shares_amount_list = ["Средневзвешенное количество выпущенных обыкновенных акций", "Средневзвешенное количество обыкновенных акций",
@@ -404,21 +240,26 @@ def find_amount(text): #find amount of shares  #TODOOOOO
         #print(line)
         if len(line) != 0:
             for name in shares_amount_list:
-                if amount_line.count(name) >= 1:
-                    #amount_line = edit_whitespaces(amount_line)
-                    print(amount_line)
-                    amount_dict = camel(filename, counter) #dict with info on the page
+                if line.count(name) >= 1:
+                    print(line)
+                    amount_dict = camel(filename, str(counter), ['90,800,550,40']) #dict with info on the page
+                    for key,value in amount_dict.items():
+                        print(key, value)
+                        if key.count(name) > 0:
+                            amount_key = key
+                            amount_value = value
+                    print(amount_key, amount_value)
                     for mu in measure_unit_thousands_list:
-                        if amount_line.count(mu) >= 1:
-                            ticker.amount = float(amount_line.strip().split("  ")[1].replace(" ", "").replace(",", "")) * 1000
+                        if amount_key.count(mu) > 0:
+                            ticker.amount = amount_value * 1000
                             #print(f"Количество акций - {ticker.amount} шт.")
                             return True
                     for mu in measure_unit_mil_list:
-                        if amount_line.count(mu) >= 1:
-                            ticker.amount = float(amount_line.strip().split("  ")[1].replace(" ", "").replace(",", "")) * 1000000
+                        if amount_key.count(mu) > 0:
+                            ticker.amount = amount_value * 1000000
                             #print(f"Количество акций - {ticker.amount} шт.")
                             return True
-                    ticker.amount = float(amount_line.strip().split("  ")[1].replace(" ", "").replace(",", ""))
+                    ticker.amount = amount_value
                     #print(f"Количество акций - {ticker.amount} шт.")
                     return True
 
